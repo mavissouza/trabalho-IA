4fd7c3e35747784bd0ad605125a448e58b02f1ed
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-08-15 09:22:09 -0300

    .

--- script.js
+++ script.js
@@ -5,12 +5,51 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-
+{
     enunciado: "Pergunta 1"
     alternativas: [
        "Alternativa 1",
        "Alternativa 2"
   ]
-   
+},
+
    "Pergunta 2"
-]
+   alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+ ]
+{,
+}
+
+   "Pergunta 3"
+   alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+ ]
+ {,
+ }
+
+   "Pergunta 4"
+   alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+ ]
+ {,
+ }
+
+   "Pergunta 5"
+   alternativas: [
+      "Alternativa 1",
+      "Alternativa 2"
+ ]
+
+ {
+ },
+];
+   
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+
+}
