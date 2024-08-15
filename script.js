7243e114a8bdb2d3ad4bcb777c0f0eec3cd0f219
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-08-15 10:37:02 -0300

    .

--- script.js
+++ script.js
@@ -12,39 +12,38 @@ const perguntas = [
        "Alternativa 2"
   ]
 },
+{
 
    "Pergunta 2"
    alternativas: [
       "Alternativa 1",
       "Alternativa 2"
  ]
-{,
-}
+},
+{
 
    "Pergunta 3"
    alternativas: [
       "Alternativa 1",
       "Alternativa 2"
  ]
- {,
- }
+},
+{
 
    "Pergunta 4"
    alternativas: [
       "Alternativa 1",
       "Alternativa 2"
  ]
- {,
- }
+},
+{
 
    "Pergunta 5"
    alternativas: [
       "Alternativa 1",
       "Alternativa 2"
  ]
-
- {
- },
+ }
 ];
    
 let atual = 0;
